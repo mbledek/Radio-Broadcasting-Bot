daf541fd04cd419758ede11f3eac6cd1a499bb4b
Blender <63952420+mbledek@users.noreply.github.com>
2023-03-25 14:50:18 +0100

    Small changes and fixes

--- bot/config.py
+++ bot/config.py
@@ -4,5 +4,6 @@ SPOTIPY_CLIENT_ID = ''
 SPOTIPY_CLIENT_SECRET = ''
 
 admin_role = 0
+proposition_channel - 0
 
-spotify_random_playlist = 'https://open.spotify.com/playlist/0jH70qZn1b4pBva0Xj0rk6'
+default_playlist = '0jH70qZn1b4pBva0Xj0rk6'
