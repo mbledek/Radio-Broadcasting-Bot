8cf77a2caf07801bae0377dadd090fc26178c63b
Blender <63952420+mbledek@users.noreply.github.com>
2023-03-29 15:12:26 +0200

    Major fixes

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import asyncio
 from abc import ABC
 import threading
 import discord
+import spotipy
 from discord.ext import commands
 from logzero import logfile, logger
+from datetime import datetime, date
 
 from bot.radiowezel import Radio
-from bot.config import TOKEN
-from bot.spotify import spotify_list
+from bot.config import TOKEN, default_playlist
+from bot.spotify import spotify_list, queue_random, skip_song
 
 
 class Radiowezel(commands.Bot, ABC):
@@ -21,29 +24,29 @@ class Radiowezel(commands.Bot, ABC):
         self.add_cog(Radio(self))
         self.run(TOKEN)
 
-        async def on_ready(self):
-            logger.info(f'We have logged in as {self.user}')
-            spotify_list_thread = threading.Thread(target=spotify_list)
-            spotify_list_thread.start()
+    async def on_ready(self):
+        logger.info(f'We have logged in as {self.user}')
+        spotify_list_thread = threading.Thread(target=spotify_list)
+        spotify_list_thread.start()
 
-            await self.change_presence(activity=discord.Game(name="https://github.com/mbledek"))
+        await self.change_presence(activity=discord.Game(name="https://github.com/mbledek"))
 
-            # Start music at round hours
-            played = False
-            while True:
-                if 0 <= date.today().weekday() <= 4:
-                    if 10 <= datetime.now().hour <= 12 and datetime.now().minute == 1 and not played:
-                        try:
-                            queue_random(default_playlist, count=5)
-                            await asyncio.sleep(5)
-                            skip_song()
-                            played = True
-                        except spotipy.exceptions.SpotifyException:
-                            logger.error("Aplikacja Spotify jest wyłączona")
-                    elif 10 <= datetime.now().hour <= 12 and datetime.now().minute == 2 and played:
-                        played = False
+        # Start music at round hours
+        played = False
+        while True:
+            if 0 <= date.today().weekday() <= 4:
+                if 10 <= datetime.now().hour <= 12 and datetime.now().minute == 1 and not played:
+                    try:
+                        queue_random(default_playlist, count=5)
+                        await asyncio.sleep(5)
+                        skip_song()
+                        played = True
+                    except spotipy.exceptions.SpotifyException:
+                        logger.error("Aplikacja Spotify jest wyłączona")
+                elif 10 <= datetime.now().hour <= 12 and datetime.now().minute == 2 and played:
+                    played = False
 
-                await asyncio.sleep(45)
+            await asyncio.sleep(45)
 
 
 Radiowezel()
